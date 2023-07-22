2a749d820c7d06191677eb292e683c1431b432ec
Hakan <hakan.gul@daimia.com>
2023-07-22 03:45:56 +0300

    clear-code

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const textarea= document.querySelector('textarea');
+const textarea = document.querySelector('textarea');
 const totalChar = document.querySelector('#total-char');
 const remChar = document.querySelector('#rem-char');
-
-
 
 textarea.addEventListener("keyup",updateCounter)
 
 function updateCounter(){
     let val = textarea.value.length;
-    if(val<=140){
-        totalChar.textContent= val;
-        remChar.textContent= textarea.getAttribute('maxlength') - val;
-    }  
+    totalChar.textContent= val;
+    remChar.textContent= textarea.getAttribute('maxlength') - val;
 }
 
 updateCounter();
